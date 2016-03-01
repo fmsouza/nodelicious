0caf1be9037cdbf9011c42d4380b2d5a601809f4
Fred Souza <fredericoamsouza@gmail.com>
2016-03-01 10:46:52 -0300

    Fixing db config variable name

--- src/config.js
+++ src/config.js
@@ -6,15 +6,15 @@
 module.exports = {
     root: __dirname,
     logs: {
-        runtime: '/tmp/runtime.log',
-        server: '/tmp/server.log'
+        runtime: '../runtime.log',
+        server: '../server.log'
     },
     server: {
         ip: '0.0.0.0',
         port: 8081
     },
     database: {
-        dbName: process.env.NoDEJS_DB_NAME  || 'nodejs',
+        dbName: process.env.NODEJS_DB_NAME  || 'nodejs',
         host: process.env.NODEJS_DB_HOST    || 'localhost',
         port: process.env.NODEJS_DB_PORT    || 27017,
         user: process.env.NODEJS_DB_USER    || '',
