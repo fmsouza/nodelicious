712996f66b91c7dd1eb19349036200073304fdc3
Fred Souza <fredericoamsouza@gmail.com>
2016-03-06 17:28:09 -0300

    Improving logging

--- src/config.js
+++ src/config.js
@@ -1,13 +1,18 @@
+'use strict';
 /* global __dirname, process; */
 /**
  * Application configuration
  * You may use it to describe every global configuration data
  */
+let data = new Date();
+let time = `${data.getFullYear()}-${data.getMonth()}-${data.getDate()}`;
+let logBasePath = process.env.NODEJS_LOG_PATH || './logs';
+
 module.exports = {
     root: __dirname,
     logs: {
-        runtime: '../runtime.log',
-        server: '../server.log'
+        runtime: `${logBasePath}/runtime/${time}.log`,
+        server: `${logBasePath}/server/${time}.log`
     },
     server: {
         ip: '0.0.0.0',
